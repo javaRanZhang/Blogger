6c71cd489f1b56e8196a83b8e050bce3fbda9831
dllo <908096626@qq.com>
2017-08-23 17:58:56 +0800

    完整各个模块 写了添加(注册或者发布博客)与查询(登录与看博客) 体现出了一对多的概念

--- src/main/java/com/lanou/service/UserServiceImpl.java
+++ src/main/java/com/lanou/service/UserServiceImpl.java
@@ -1,7 +1,22 @@
 package com.lanou.service;
 
+import com.lanou.bean.User;
+import com.lanou.mapper.UserMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
 /**
  * Created by dllo on 17/8/23.
  */
-public class UserServiceImpl {
+@Service
+public class UserServiceImpl implements UserService {
+    @Resource
+    private UserMapper userMapper;
+
+    public  void insertUser(User user){
+        System.out.println(111);
+        userMapper.insertUser(user);
+        System.out.println(222);
+    }
 }
